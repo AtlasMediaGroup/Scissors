1a748f754c5a0b92d9bbf2e951753f21d651a840
Video <videogamesm12@gmail.com>
2022-03-20 06:32:37 -0600

    Downgrades Paperclip to 2.0.1
    Now you can actually create functional paperclip jars.

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,7 @@ repositories {
 dependencies {
     remapper("net.fabricmc:tiny-remapper:0.8.1:fat")
     decompiler("net.minecraftforge:forgeflower:1.5.498.22")
-    paperclip("io.papermc:paperclip:3.0.2")
+    paperclip("io.papermc:paperclip:2.0.1")
 }
 
 allprojects {
