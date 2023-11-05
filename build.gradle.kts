c454da9e1a6b7d8cfbf961d57e52fd229fd953c7
Telesphoreo <me@telesphoreo.me>
2023-11-05 14:08:29 -0600

    Java 21 support

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,7 @@ repositories {
 }
 
 dependencies {
-    remapper("net.fabricmc:tiny-remapper:0.8.9:fat")
+    remapper("net.fabricmc:tiny-remapper:0.8.11:fat")
     decompiler("net.minecraftforge:forgeflower:2.0.629.1")
     paperclip("io.papermc:paperclip:3.0.4-SNAPSHOT")
 }
